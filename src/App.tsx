ee5fd47c31365b747e7db8109495b1dec6c957e4
Midnight-Time <p.v.filipenko.work@gmail.com>
2023-12-16 14:21:53 +0300

    loading screen added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import { useEffect } from "react";
+import { gsap } from "gsap/gsap-core";
 
 function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      gsap
+        .timeline()
+        .to(".preloader", {
+          opacity: 0,
+          duration: 1,
+          ease: "Power3.easeOut",
+        })
+        .to(".preloader", { display: "none" });
+    }, 200);
+  }, []);
+
   return (
     <div className="app">
       <HomePage />
